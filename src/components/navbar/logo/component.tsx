cf3bc45e606b98a804e96ff51fee8c3a659a5e5b
harmanacc <armanabdiacc@gmail.com>
2025-10-13 04:32:45 +0330

    logo

--- src/components/navbar/logo/component.tsx
+++ src/components/navbar/logo/component.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export function Logo() {
   return (
-    <Link href="/" className="text-xl font-bold text-gray-900 dark:text-white">
+    <Link
+      href="/"
+      className="text-xl flex font-bold text-gray-900 dark:text-white md:pl-20 gap-4 items-center justify-center"
+    >
+      <Image src="/logo.jpg" alt="Logo" width={50} height={50} />
       ShorOOt
     </Link>
   );
